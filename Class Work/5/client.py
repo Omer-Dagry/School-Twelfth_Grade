b380487cca4e93476aa7a3221acb4e967e110374
Omer-Dagry <93447258+Omer-Dagry@users.noreply.github.com>
2022-09-29 16:17:29 +0300

    client brute force
    finished brute force for client
    
    need to code server and the client connection to the server

--- Class Work/5/client.py
+++ Class Work/5/client.py
@@ -3,48 +3,122 @@ import itertools
 import socket
 import multiprocessing
 import queue
-import string
+from typing import *
 
 
 IP = "127.0.0.1"
 PORT = 8820
 PACKET_LEN = 32
-ABC_LOW = "abcdefghijklmnopqrstuvwxyz"
-ABC_HIGH = ABC_LOW.upper()
 NUMBERS = "0123456789"
-SIGNS = "!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ " + '"'
-ALL = string.printable
+CPU_COUNT = multiprocessing.cpu_count() - 1
+LEN_OF_MD5_HASHED_DATA = 10
 
 
-def brute_force_decrypt_md5(md5_hash, repeat, multiprocessing_queue):
-    message = None
-    for option in itertools.product(ALL, repeat=repeat):
-        if hashlib.md5("".join(option).encode('utf-8')).hexdigest() == md5_hash:
-            message = "".join(option)
-            break
-    if message is not None:
-        multiprocessing_queue.put(message)
+# def brute_force_decrypt_md5(md5_hash, repeat, multiprocessing_queue):
+#     message = None
+#     for option in itertools.product(NUMBERS, repeat=repeat):
+#         if hashlib.md5("".join(option).encode('utf-8')).hexdigest() == md5_hash:
+#             message = "".join(option)
+#             break
+#     if message is not None:
+#         multiprocessing_queue.put(message)
+
+
+def brute_force_decrypt_md5(md5_hash: str, base_string_length: int, multiprocessing_queue: queue.Queue,
+                            start_from: str, end_at: str, current_string: str = "") -> Union[str, None]:
+    global NUMBERS
+    # start from
+    numbers = NUMBERS[NUMBERS.index(start_from[0]):]
+    for number in numbers:
+        if base_string_length > 1:
+            # after 1 run do all the numbers
+            if numbers.index(number) != 0:
+                start_from = "0" * base_string_length
+            res = brute_force_decrypt_md5(md5_hash, base_string_length - 1, multiprocessing_queue,
+                                          start_from[1:], end_at, current_string=current_string + number)
+            if res is not None and "stop" in res and base_string_length != 10:
+                return res
+            elif res is not None and "stop" in res:
+                multiprocessing_queue.put("not found, stopped at '%s'" % res.split("top, ")[1])
+                break
+            elif res is not None and base_string_length != 10:
+                return res
+            elif res is not None:
+                multiprocessing_queue.put(res)
+                break
+        else:
+            current_string_ = current_string + number
+            if hashlib.md5(current_string_.encode()).hexdigest().lower() == md5_hash.lower():
+                return current_string_
+            if current_string_ == end_at:
+                return "stop, " + current_string_
+    if base_string_length != 10:
+        return None
+
+
+# EC9C0F7EDCC18A98B1F31853B1813301
+# 3735928559
+
+
+# start_from = "0000000000"
+# end_at = "9999999999"
+#
+# total = int(end_at) - int(start_from)
+# print((total / CPU_COUNT) % 1 > 0)
+# range_ = int(start_from)
+# for i in range(CPU_COUNT):
+#     if i == CPU_COUNT - 1:
+#         print(range_, int(end_at) + 1)
+#     else:
+#         print(range_, range_ + (total // CPU_COUNT))
+#         range_ += (total // CPU_COUNT)
+# exit()
+# # brute_force_decrypt_md5("EC9C0F7EDCC18A98B1F31853B1813301".lower(), 10, queue.Queue(), "3000015235", "3001015235")
 
 
 def main():
-    # open socket to server
-    sock = socket.socket()
-    sock.connect((IP, PORT))
-    # get my range
-    my_range = sock.recv(PACKET_LEN).decode()
-    # get md5 hash to brute force
-    md5_hash = sock.recv(PACKET_LEN).decode().lower()
+    # # open socket to server
+    # sock = socket.socket()
+    # sock.connect((IP, PORT))
+    # # get my range
+    # my_range = sock.recv(20).decode()  # start from (10 digits) & end at (10 digits)
+    # start_from = int(my_range[:10])
+    # end_at = int(my_range[10:])
+    # total = end_at - start_from
+    # # get md5 hash to brute force
+    # md5_hash = sock.recv(32).decode().lower()  # the md5 hash (32 digit)
+    start_from = 0
+    end_at = 9999999999
+    total = end_at - start_from
+    md5_hash = "EC9C0F7EDCC18A98B1F31853B1813301".lower()
     processes = []
     # communication
     multiprocessing_queue = multiprocessing.Queue()
     # open number of cores in the PC processes
-    for i in range(multiprocessing.cpu_count()):
-        repeat = None
-        p = multiprocessing.Process(target=brute_force_decrypt_md5,
-                                    args=(md5_hash, repeat, multiprocessing_queue,),
-                                    daemon=True)
-        p.start()
-        processes.append(p)
+    for i in range(CPU_COUNT):
+        if i == CPU_COUNT - 1:
+            p = multiprocessing.Process(target=brute_force_decrypt_md5,
+                                        args=(md5_hash.lower(), LEN_OF_MD5_HASHED_DATA, multiprocessing_queue,
+                                              str(start_from).rjust(10, "0"),
+                                              str(int(end_at)).rjust(10, "0")),
+                                        daemon=True)
+            p.start()
+            processes.append(p)
+            print("Thread %d Range:" % (i + 1),
+                  str(start_from).rjust(10, "0"), "-",
+                  str(int(end_at)).rjust(10, "0"))
+        else:
+            p = multiprocessing.Process(target=brute_force_decrypt_md5,
+                                        args=(md5_hash.lower(), LEN_OF_MD5_HASHED_DATA, multiprocessing_queue,
+                                              str(start_from).rjust(10, "0"),
+                                              str(start_from + (total // CPU_COUNT)).rjust(10, "0")),
+                                        daemon=True)
+            p.start()
+            processes.append(p)
+            print("Thread %d Range:" % (i + 1),
+                  str(start_from).rjust(10, "0"), "-",
+                  str(start_from + (total // CPU_COUNT)).rjust(10, "0"))
+            start_from += (total // CPU_COUNT)
     # wait for processes to return the md5 message
     # or until all processes finish
     decrypted_md5_hash = None
@@ -53,7 +127,10 @@ def main():
             if not p.is_alive() and decrypted_md5_hash is None:
                 try:
                     decrypted_md5_hash = multiprocessing_queue.get(timeout=5)
-                    print("process returned:", decrypted_md5_hash)
+                    if decrypted_md5_hash is not None and "not found" in decrypted_md5_hash:
+                        print("Process Finished And Returned: Not Found.")
+                        decrypted_md5_hash = None
+                    print("Process Finished And Found The Encrypted Data:", decrypted_md5_hash)
                     # got result close all other processes
                     for p_ in processes:
                         if p_ is not multiprocessing.current_process():
--- Class Work/5/server.py
+++ Class Work/5/server.py
@@ -1,16 +1,19 @@
+import datetime
 import itertools
 import socket
 import threading
 
 
-ABC = "abcdefghigklmnopqrstuvwxyz"
-NUMBERS = "1234567890"
-SIGNS = "!?:|\\/.,<>;'@#$%^&*(){}[]~`" + '"'
+MD5_HASH = "EC9C0F7EDCC18A98B1F31853B1813301".lower()
+NUMBERS = "0123456789"
 
 
 def main():
-    for option in itertools.product(ABC + NUMBERS, repeat=10):
+    start = datetime.datetime.now()
+    print(start)
+    for option in itertools.product(NUMBERS, repeat=10):
         pass
+    print(datetime.datetime.now() - start)
 
 
 if __name__ == '__main__':
