ca1a9ff08e905fff3d4aaf689f573cedd2548ffb
Omer-Dagry <93447258+Omer-Dagry@users.noreply.github.com>
2023-05-06 03:08:45 +0300

    calls udp

--- Project/Whatsapp/calls/calls_udp.py
+++ Project/Whatsapp/calls/calls_udp.py
@@ -1,3 +1,5 @@
+import threading
+import time
 import socket
 import pyaudio
 import traceback
@@ -9,12 +11,50 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
 # Server
-HOST = None
-PORT = 8822
+HOST = "127.0.0.1"
+PORT = 16400
 BUFFER_SIZE = CHUNK * 4
+
+# Globals
+stop = False
+
+
+def get_sound(stream: pyaudio.Stream, client_socket: socket.socket):
+    global stop
+    while not stop:
+        try:
+            # write audio received from server to the stream
+            data = client_socket.recvfrom(BUFFER_SIZE)[0]
+        except socket.timeout:
+            continue
+        except Exception as e:
+            traceback.print_exception(e)
+            stop = True
+            break
+        if data != b"":
+            # print(data)
+            stream.write(data)
+
+
+def send_sound(stream: pyaudio.Stream, client_socket: socket.socket):
+    global stop
+    while not stop:
+        # Read audio data from the stream
+        data = stream.read(CHUNK)
+        try:
+            # Send the audio data to the server
+            client_socket.sendto(data, (HOST, PORT))
+        except socket.timeout:
+            pass
+        except Exception as e:
+            traceback.print_exception(e)
+            stop = True
+            break
+        time.sleep(0.02)
 
 
 def main():
+    global stop
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # Create PyAudio stream for recording and playing audio
     p = pyaudio.PyAudio()
@@ -25,25 +65,14 @@ def main():
     # output_devices = [device for device in devices
     #                   if "maxOutputChannels" in device and device["maxOutputChannels"] > 0]
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK)
+    send_sound_thread = threading.Thread(target=send_sound, args=(stream, client_socket,), daemon=True)
     try:
-        client_socket.settimeout(0.01)
-        while True:
-            try:
-                # Read audio data from the stream
-                data = stream.read(CHUNK)
-                # Send the audio data to the server
-                client_socket.sendto(data, (HOST, PORT))
-                # write audio received from server to the stream
-                data = client_socket.recvfrom(BUFFER_SIZE)[0]
-                if data != b"":
-                    # print(data)
-                    stream.write(data)
-            except socket.timeout:
-                pass
-            except Exception as e:
-                traceback.print_exception(e)
-                break
+        client_socket.settimeout(0.1)
+        send_sound_thread.start()
+        get_sound(stream, client_socket)
     finally:
+        stop = True
+        send_sound_thread.join(1)
         # Clean up PyAudio and close the connection
         stream.stop_stream()
         stream.close()
--- Project/Whatsapp/calls/calls_udp_server.py
+++ Project/Whatsapp/calls/calls_udp_server.py
@@ -1,57 +1,77 @@
+import queue
 import socket
 import datetime
-import traceback
+import multiprocessing
 
 
 HOST = 'localhost'
-PORT = 8822
+PORT = 16400
 CHUNK = 1024 * 2
 BUFFER_SIZE = CHUNK * 4
 
 # Globals
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_socket.bind(("0.0.0.0", PORT))
-clients = {}
 stop: None | datetime.datetime = None
 
 
 # Function to broadcast audio stream to all connected clients
-def broadcast_audio(data: bytes, sent_from: tuple):
-    remove = []
-    for addr in clients.keys():
-        try:
-            if (datetime.datetime.now() - clients[addr]).seconds > 5:
-                raise TimeoutError
-            if addr != sent_from and data != b"":
-                server_socket.sendto(data, addr)
-        except TimeoutError:
-            print(f"{addr} timed out")
-            remove.append(addr)
-        except Exception as e:
-            print(f"closed {addr}, ({str(e)})")
-            traceback.print_exception(e)
-            remove.append(addr)
-    for addr in remove:
-        clients.pop(addr)
+def broadcast_audio(data_queue: multiprocessing.Queue,
+                    server_socket: socket.socket, remove_queue: multiprocessing.Queue):
+    while True:
+        data, sent_from, clients = data_queue.get()
+        for addr in clients.keys():
+            try:
+                if (datetime.datetime.now() - clients[addr]).seconds > 5:
+                    raise TimeoutError
+                if addr != sent_from and data != b"":
+                    server_socket.sendto(data, addr)
+            except TimeoutError:
+                print(f"{addr} timed out")
+                remove_queue.put(sent_from)
+            except Exception as e:
+                print(f"closed {addr}, ({str(e)})")
+                remove_queue.put(sent_from)
 
 
 def main():
     global stop
+    clients = {}
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(("0.0.0.0", PORT))
+    #
+    data_queue = multiprocessing.Queue()
+    remove_queue = multiprocessing.Queue()
+    broadcast_audio_thread = multiprocessing.Process(
+        target=broadcast_audio, args=(data_queue, server_socket, remove_queue), daemon=True
+    )
+    broadcast_audio_thread.start()
+    #
     server_socket.settimeout(0.01)
     stop = datetime.datetime.now()
-    while clients.keys() or (datetime.datetime.now() - stop).seconds < 5:
+    last_msg = stop
+    while (clients.keys() or (datetime.datetime.now() - stop).seconds < 20) and \
+            (len(clients.keys()) <= 1 and (datetime.datetime.now() - last_msg).seconds < 20):
         try:
             data, addr = server_socket.recvfrom(BUFFER_SIZE)
             if addr not in clients:
                 print(f"new connection from {addr}")
-            clients[addr] = datetime.datetime.now()
-            broadcast_audio(data, addr)
+            time = datetime.datetime.now()
+            clients[addr] = time
+            last_msg = time
+            data_queue.put((data, addr, clients))
         except ConnectionResetError:
             pass
         except socket.timeout:
-            broadcast_audio(b"", (None, None))
+            pass
         except Exception as e:
-            traceback.print_exception(e)
+            # traceback.print_exception(e)
+            pass
+        try:
+            remove = data_queue.get(block=False)
+            print(remove)
+            if remove in clients:
+                clients.pop(remove)
+        except queue.Empty:
+            pass
 
 
 if __name__ == '__main__':
