9bb29f9bab4d9d2628c7323197d387a327244637
Omer-Dagry <93447258+Omer-Dagry@users.noreply.github.com>
2023-05-24 05:27:45 +0300

    added author and date

--- Project/Whatsapp/Server/SyncDB/sync_database.py
+++ Project/Whatsapp/Server/SyncDB/sync_database.py
@@ -1,3 +1,11 @@
+"""
+###############################################
+Author: Omer Dagry
+Mail: omerdagry@gmail.com
+Date: 30/05/2023 (dd/mm/yyyy)
+###############################################
+"""
+
 import os
 import pickle
 import threading
